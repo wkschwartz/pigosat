792a8929f67b6fe22068c405bec9673ff4517e8d
William Schwartz <wkschwartz@gmail.com>
2015-04-12 23:25:55 -0400

    Abort building Go slice early if C array pointer is bad or empty

--- pigosat.go
+++ pigosat.go
@@ -320,6 +320,9 @@ func (p *Pigosat) FailedAssumptions() []Literal {
 // Converts a 0-terminated array of literal results to a slice.
 // Does not acquire internal locks.
 func (p *Pigosat) litArrayToSlice(litPtr *C.int) []Literal {
+	if litPtr == C.NULL || *litPtr == 0 {
+		return []Literal{}
+	}
 	// It should be reasonable to use the number of vars in
 	// the solver as the max array size, since we aren't tracking
 	// the active number of assumptions.

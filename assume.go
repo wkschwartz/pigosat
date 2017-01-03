d16d3af94d7a3194e618b5ba5619997a0e7f7c6a
William Schwartz <wkschwartz@gmail.com>
2017-01-02 21:47:24 -0600

    Simplify Assume documentation and target it toward Go and PiGoSAT.

--- assume.go
+++ assume.go
@@ -12,31 +12,24 @@ var cArray123 = [3]C.int{1, 2, 3}
 var cArray1230 = [4]C.int{1, 2, 3, 0}
 var cZero C.int = 0
 
-// Assume adds a unit clause (i.e., a clause containing one literal) containing
-// just the given literal for just the next call to Solve. You can add arbitrary
-// many assumptions before the next Solve. After the next call to Solve, the
-// assumptions are removed.
-//
-// More precisely, assumptions actually remain valid even after the next call to
-// Solve() returns. Valid means they remain 'assumed' until a call to
-// AddClauses(), Assume(), or another Solve(), following the first Solve().
-// They need to stay valid for FailedAssumptions(), etc., to work correctly.
+// Assume adds a temporary unit clause, i.e., a clause containing the one
+// literal you pass as an argument. An assumption remains valid after the next
+// call to Solve returns until a call to AddClauses, Assume, or a second Solve.
+// You can add arbitrary many assumptions before the next call to Solve.
+// Methods FailedAssumptions, FailedAssumptions, MaxSatisfiableAssumptions, and
+// NextMaxSatisfiableAssumptions operate on the current, valid assumptions.
 func (p *Pigosat) Assume(lit Literal) {
 	defer p.ready(false)()
 	// void picosat_assume (PicoSAT *, int lit);
 	C.picosat_assume(p.p, C.int(lit))
 }
 
-// FailedAssumption returns true if the literal is a failed assumption, meaning
-// the literal was added with Assume then used to derive unsatisfiability.
-// The return value is only meaningful as long the current assumptions are
-// valid. See Assume() for more details.
-//
-// From the Picosat documentation:
-//   This is as accurate as generating core literals, but still of course is an
-//   overapproximation of the set of assumptions really necessary. The technique
-//   does not need clausal core generation nor tracing to be enabled and thus
-//   can be much more effective.
+// FailedAssumption returns whether a given literal is a valid, "failed"
+// assumption (see Assume), meaning the last call to Solve used the assumption
+// to derive unsatisfiability. FailedAssumption always returns false when the
+// last call to Solve had status other than Unsatisfiable. This may be an
+// over-approximation of the set of assumptions necessary to derive
+// unsatisfiability.
 func (p *Pigosat) FailedAssumption(lit Literal) bool {
 	defer p.ready(true)()
 	// picoast_failed_assumption SIGABRTs if the following conditional is true
@@ -65,11 +58,10 @@ func litArrayToSlice(litPtr *C.int, maxLen int) []Literal {
 	return lits
 }
 
-// FailedAssumptions returns a list of failed assumptions in the last call to
-// Solve() or an empty list if the last call to Solve() did not return
-// Unsatisfiable. See Assume() and FailedAssumption() for more details.
+// FailedAssumptions returns a list of failed assumptions, i.e., all the
+// for which FailedAssumption returns true. See FailedAssumption.
 func (p *Pigosat) FailedAssumptions() []Literal {
-	defer p.ready(true)()
+	defer p.ready(false)() // Overwrites what becomes litPtr below.
 	if p.res() != Unsatisfiable {
 		return []Literal{}
 	}
@@ -80,52 +72,31 @@ func (p *Pigosat) FailedAssumptions() []Literal {
 }
 
 // MaxSatisfiableAssumptions computes a maximal subset of satisfiable
-// assumptions. See Assume() for more details. You need to set the assumptions
-// and call Solve() before calling this function. The result is a list of
+// assumptions. See Assume's documentation. You need to set the assumptions
+// and call Solve() before calling this method. The result is a list of
 // assumptions that consistently can be asserted at the same time.
-//
-// It could be beneficial to set the default phase of assumptions
-// to true (positive). This can speed up the computation.
 func (p *Pigosat) MaxSatisfiableAssumptions() []Literal {
 	defer p.ready(false)()
 	if C.picosat_inconsistent(p.p) != 0 {
 		return []Literal{}
 	}
-	//const int * picosat_maximal_satisfiable_subset_of_assumptions (PicoSAT *);
+	// const int * picosat_maximal_satisfiable_subset_of_assumptions (PicoSAT *);
 	litPtr := C.picosat_maximal_satisfiable_subset_of_assumptions(p.p)
 	return litArrayToSlice(litPtr, int(C.picosat_variables(p.p)))
 }
 
-// This function assumes that you have set up all assumptions with
-// 'Assume()'.  Then it calls 'Solve()' internally unless the
-// formula is already inconsistent without assumptions, i.e.  it contains
-// the empty clause.  After that it extracts a maximal satisfiable subset of
-// assumptions.
-//
-// The result is a zero terminated maximal subset of consistent assumptions
-// or an empty list if the formula contains the empty clause and thus no
-// more maximal consistent subsets of assumptions can be extracted.  In the
-// first case, before returning, a blocking clause is added, that rules out
-// the result for the next call.
-//
-// NOTE: adding the blocking clause changes the CNF.
-//
-// So the following idiom
-//
-// var lits []Literal;
-// p.Assume(a1)
-// p.Assume(a2)
-// p.Assume(a3)
-// p.Assume(a4)
-// for mss := p.NextMaxSatisfiableAssumptions(); len(mss) > 0; mss = p.NextMaxSatisfiableAssumptions() {
-//     ProcessResults(mss)
-// }
-//
-// can be used to iterate over all maximal consistent subsets of
-// the set of assumptions {a1,a2,a3,a4}.
-//
-// It could be beneficial to set the default phase of assumptions
-// to true (positive). This might speed up the computation.
+// NextMaxSatisfiableAssumptions is like MaxSatisfiableAssumptions, but operates
+// like an iterator to give a different maximal subset of satisfiable
+// assumptions upon each call. To do this, it internally calls Solve followed by
+// BlockSolution, which modifies the underlying formula (thus changing the
+// result of AddedOriginalClauses), and then reassuming the solutions that were
+// valid when you called NextMaxSatisfiableAssumptions. Use it as follows.
+// First, set your formula and assumptions using AddClauses and Assume. Then
+// iterate over the different maximal satisfiable subsets of assumptions with:
+//    var mss []Literal
+//    for mss := p.NextMaxSatisfiableAssumptions(); len(mss) > 0; mss = p.NextMaxSatisfiableAssumptions() {
+//        // Do stuff with mss
+//    }
 func (p *Pigosat) NextMaxSatisfiableAssumptions() []Literal {
 	defer p.ready(false)()
 	if C.picosat_inconsistent(p.p) != 0 {
